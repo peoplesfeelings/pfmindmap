a8c7a56c4bc8ecfd08b6a3e00a486fa9f16ec0b7
my name <my@email.com>
2021-06-15 01:29:55 -0400

    (rollup) changed output to es format

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'src/main.js',
-  output: {
-    exports: 'default',
-    file: 'dist/pfmindmap_umd.js',
-    format: 'umd',
-    name: 'pfmindmap'
-  },
-  plugins: [nodeResolve()]
-};
+export default [
+  {
+    input: 'src/main.js',
+    output: {
+      exports: 'default',
+      file: 'dist/pfmindmap_esm.js',
+      format: 'es',
+      name: 'pfmindmap'
+    },
+    plugins: [nodeResolve()]
+  }
+];
