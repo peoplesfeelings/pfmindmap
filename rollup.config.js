6fc2843a414ecac515b4617b139035a6a28a5ae3
my name <my@email.com>
2021-06-16 23:52:11 -0400

    (rollup) rollup config now generates a dependencies file, and prepends a banner to the bundle. also updated readme for new installation methods.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,37 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import banner from 'rollup-plugin-banner';
+import path from 'path';
+import license from 'rollup-plugin-license';
+
+const bannerText = `
+pfmindmap (github.com/peoplesfeelings/pfmindmap)
+copyright people's feelings 2021
+
+bundled with dependencies, including:
+D3 (https://github.com/d3/d3)
+Observable Runtime (https://github.com/observablehq/runtime)
+
+see dependencies.txt, built with this bundle, for dependency licenses and copyrights
+`;
 
 export default [
   {
     input: 'src/main.js',
     output: {
       exports: 'default',
-      file: 'dist/pfmindmap_esm.js',
+      file: 'dist/pfmindmap_bundle.js',
       format: 'es',
       name: 'pfmindmap'
     },
-    plugins: [nodeResolve()]
+    plugins: [
+      nodeResolve(),
+      banner(bannerText),
+      license({
+        thirdParty: {
+          output: path.join(__dirname, 'dist', 'dependencies.txt'),
+          includePrivate: true, 
+        },
+      })
+    ]
   }
 ];
