f10751922bfa9fa88dd0267de14fa957c3baa9ab
my name <my@email.com>
2021-06-21 16:38:46 -0400

    (rollup) rollup now puts license & copyright for this module and its dependencies in a banner at the top of the bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,30 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import banner from 'rollup-plugin-banner';
 import path from 'path';
-import license from 'rollup-plugin-license';
+import rpl from 'rollup-plugin-license';
 
 const bannerText = `
-pfmindmap (github.com/peoplesfeelings/pfmindmap)
+This file is a bundle of <%= pkg.name %> <%= pkg.version %> and its dependencies.
 
-bundled with dependencies, including:
-D3 (https://github.com/d3/d3)
-Observable Runtime (https://github.com/observablehq/runtime)
+<%= pkg.name %> <%= pkg.version %>
+copyright 2021 people's feelings
+pseudonym hash: 2A73BD701A1CE2FBE2CB3B4D119E9DFC061AA38C26763BAE921FB87336E87E42
 
-see dependencies.txt, built with this bundle, for dependency licenses and copyrights
-see LICENSE.txt, part of pfmindmap repo, for license/copyright of this module
+github.com/peoplesfeelings/pfmindmap
+
+"2-Clause BSD"
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+Dependencies:
+
+<% _.forEach(dependencies, function (dependency) { %>
+  <%= dependency.name %> -- <%= dependency.version %>
+  <%= dependency.licenseText %>
+<% }) %>
 `;
 
 export default [
@@ -25,10 +38,10 @@ export default [
     },
     plugins: [
       nodeResolve(),
-      banner(bannerText),
-      license({
+      rpl({
+        banner: bannerText,
         thirdParty: {
-          output: path.join(__dirname, 'dist', 'dependencies.txt'),
+          output: path.join(__dirname, 'dist', 'dependencies_bundled.txt'),
           includePrivate: true, 
         },
       })
